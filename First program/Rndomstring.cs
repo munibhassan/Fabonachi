04c17d3c8b1fd353bb0e08607c99c2d9938118d5
Naumanlatif1994 <naumanlatif99@gmail.com>
2022-11-14 18:12:18 +0500

    Creation Of Logic For Munna Array

--- First program/Rndomstring.cs
+++ First program/Rndomstring.cs
@@ -18,43 +18,79 @@ public class RandomStringInArraySample
     {
         string[,] roomString = new string[4, 4];
         int count = 0;
-        string horiz = "   ";
-        string verticle = "   ";
+        string rep = "";
+        string[,] horiz = new string[4, 4];
+        string[,] verticle = new string[4, 4];
+        string[,] munnaarray = new string[8, 8];
+        Console.WriteLine("Random  sequence is  ");
+        int Eight_Row = 0;
+        int Eight_Col = 0;
+
         for (int i = 0; i < 4; i++)
         {
             for (int j = 0; j < 4; j++)
             {
 
                 roomString[i, j] = checkstring();
-                roomString[j, i] = checkstring();
-
                 Console.Write(roomString[i, j] + " ");
-                // Console.Write(roomString[j, i] + " ");
 
-                // Console.WriteLine();
+                //horiz[i,j] = roomString[i, j];
+                if ((i < 4) && (j < 4))
+                {
+                    munnaarray[Eight_Row, Eight_Col] = roomString[i, j];
+                    Console.Write(munnaarray[Eight_Row, Eight_Col] + " ");
+                    Eight_Col++;
+                    if (Eight_Col == 8)
+                    {
+                        Eight_Col = 0;
+                    }
+                }
+
+            }
+            Eight_Row++;
+            if (Eight_Row == 8)
+            {
+                Eight_Row = 0;
+            }
+            Console.WriteLine();
+
+        }
+        Console.WriteLine();
+        Console.WriteLine("Verticle sequence is  ");
+        // horiz=verticle;
+        // Array.Resize(ref munnaarray,);
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
 
 
-                horiz += roomString[i, j];
-                verticle += roomString[j, i];
+                verticle[i, j] = horiz[j, i];
+
+                Console.Write(verticle[i, j] + " ");
+
+
 
 
             }
+
             Console.WriteLine();
 
         }
 
 
-        Console.WriteLine("Horizental sequence is " + horiz + " ");
-        count = Matchstring(horiz, count);
+        Console.WriteLine("Verticle sequence is  ");
 
+        Console.WriteLine();
+        //  count = Matchstring(verticle, count);
         Console.WriteLine("Total count of SOS : " + count);
+        Console.WriteLine();
 
-        Console.WriteLine("Verticle sequence is " + verticle + " ");
-        count = Matchstring(verticle, count);
+        Console.WriteLine("Horizental sequence is  ");
 
-        Console.WriteLine("Total count of SOS : " + count);
-         horiz = "";
-        verticle = "";
+        Console.WriteLine();
+        // horiz = "";
+        // verticle = "";
     }
     public int Matchstring(string check, int count = 0)
     {
--- First program/StarPattern.cs
+++ First program/StarPattern.cs
@@ -9,7 +9,75 @@ namespace First_program
     public class StarPattern
     {
 
+//        string[,] roomString = new string[4, 4];
+//        int count = 0;
+//        string horiz = "   ";
+//        string verticle = "   ";
+//        Console.WriteLine("Random  sequence is  ");
+//        for (int i = 0; i< 4; i++)
+//        {
+//            for (int j = 0; j< 4; j++)
+//            {
+
+//                roomString[i, j] = checkstring();
+//        // roomString[j, i] = checkstring();
+
+//        Console.Write(roomString[i, j] + " ");
+//                // Console.Write(roomString[j, i] + " ");
+
+//                // Console.WriteLine();
 
 
-    }
+//                horiz += roomString[i, j];
+//               // verticle += roomString[j, i];
+
+
+//            }
+//    Console.WriteLine();
+
+//        }
+//Console.WriteLine();
+//Console.WriteLine("Verticle sequence is  ");
+//for (int i = 0; i < 4; i++)
+//{
+//    for (int j = 0; j < 4; j++)
+//    {
+
+//        Console.Write(roomString[j, i] + " ");
+
+
+//        verticle += roomString[j, i];
+
+//    }
+
+//    Console.WriteLine();
+
+//}
+//Console.WriteLine();
+//count = Matchstring(verticle, count);
+//Console.WriteLine("Total count of SOS : " + count);
+//Console.WriteLine();
+
+//Console.WriteLine("Horizental sequence is  ");
+//for (int i = 0; i < 4; i++)
+//{
+//    for (int j = 0; j < 4; j++)
+//    {
+
+//        Console.Write(roomString[i, j] + " ");
+
+
+//        verticle += roomString[i, j];
+
+//    }
+
+//    Console.WriteLine();
+
+//}
+//Console.WriteLine();
+//horiz = "";
+//verticle = "";
+
+
+}
 }
