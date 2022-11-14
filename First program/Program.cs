2d1d2fac420989fe9d5bfba943a5137be3fe70d7
Naumanlatif1994 <naumanlatif99@gmail.com>
2022-11-14 11:54:48 +0500

    New Code

--- First program/Program.cs
+++ First program/Program.cs
@@ -7,72 +7,75 @@ namespace FirstProgram
     {
         public string RandomString()
         {
-            Random ran1 = new Random();
-            String val = "";
-            string values1 = "SOS no SOS";
-            
-            int length = 1;
-            for (int i = 0; i < length; i++)
+            string chars = "OS";
+            var stringChars = new char[1];
+            Random random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
             {
-                int a = ran1.Next(1);
-                val = values1 + values1.ElementAt(a);
-                Console.WriteLine(val);
+                stringChars[i] = chars[random.Next(0, 2)];
             }
 
-            return val;
+            var randomStr = new String(stringChars);
+            Console.WriteLine(randomStr);
+            return randomStr;
+
         }
         public void method()
         {
-            string[,] roomString = new string[4, 4];
-
-
-
             string check = "SOS";
+            string[,] roomString = new string[2, 2];
 
             string match = "";
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 2; i++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < 2; j++)
                 {
-                    Console.Write(roomString[i,j] + " " + RandomString());
+                    roomString[i, j] = RandomString();
+
+                    Console.WriteLine(roomString[i, j]);
+
+
+                    // Console.Write(roomString[i, j] + " ,");
+                    // roomString[i, j] = RandomString();
+
+
                 }
+                //     match = RandomString();
                 Console.WriteLine();
-                for (int k = 0; k < 4; k++)
-                {
-
-                    match += roomString[i, 0];
-                }
+                //    Console.WriteLine("Nouman check " + match);
+                //    if (match.Contains(check))
+                //    {
+                //        Console.ForegroundColor
+                //    = ConsoleColor.Blue;
+                //        Console.WriteLine("SOS  Found");
+                //    }
+                //    else
+                //    {
+                //        Console.ForegroundColor
+                //  = ConsoleColor.Red;
+                //        Console.WriteLine("SOS not Found");
+                //    }
             }
-           
-            Console.WriteLine("Nouman check " + match);
-            if (match.Contains(check))
-            {
-                Console.WriteLine("SOS  Found");
-            }
-            else
-            {
-                Console.WriteLine("SOS not Found");
-            }
-
 
 
 
 
 
         }
-    
-
-        static void Main(string[] args)
-
-        {
-            Program p = new Program();
-            p.RandomString();
-           // p.method();
-           
 
 
-           
+        //static void Main(string[] args)
 
-        }
+        //{
+        //    Program p = new Program();
+        //// p.RandomString();
+        //  p.method();
+
+
+
+
+
+        //}
     }
 }
---
+++ First program/Rndomstring.cs
@@ -0,0 +1,77 @@
+﻿using FirstProgram;
+using System;
+public class RandomStringInArraySample
+{
+    public string checkstring()
+    {
+        string[] authors = { "S", "O" };
+        Random rand = new Random();
+
+        int index = rand.Next(authors.Length);
+        // Display the result.  
+        //  Console.WriteLine(authors[index]);
+
+        return authors[index];
+
+    }
+    public void method()
+    {
+        string[,] roomString = new string[4, 4];
+        int count = 0;
+        string check = "   ";
+        string verticle = "   ";
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+
+                roomString[i, j] = checkstring();
+                Console.Write(roomString[i, j] + " ");
+
+
+                check += roomString[i, j];
+
+                //  verticle += roomString[j, i];
+
+            }
+            Console.WriteLine(" ");
+
+
+        }
+        count = Matchstring(check, count);
+
+        count = Matchstring(verticle, count);
+        Console.WriteLine("Total count of SOS : " + count);
+
+        verticle = "";
+        check = "";
+    }
+    public int Matchstring(string check, int count = 0)
+    {
+        string match = "SOS";
+
+
+        if (check.Contains(match))
+        {
+
+            count++;
+
+            Console.WriteLine(" SOS  Found " + count);
+
+
+
+        }
+        else
+        { Console.WriteLine(" SOS not Found"); }
+        return count;
+
+    }
+
+    public static void Main()
+    {
+        RandomStringInArraySample p = new RandomStringInArraySample();
+        // p.checkstring();
+        p.method();
+
+    }
+}
