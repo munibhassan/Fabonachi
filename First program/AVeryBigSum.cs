ed287726085f6d1b07abd231184ed29a57ae02da
Munib Hassan <munib.nexus@gmail.com>
2023-03-17 00:38:57 +0500

    A Very Big Sum

---
+++ First program/AVeryBigSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_program
+{
+    public class AVeryBigSum
+    {
+        public static long aVeryBigSumFunc(List<long> ar)
+        {
+            long sum = 0;
+            foreach (int num in ar)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+
+    }
+}
--- First program/TripletCompare.cs
+++ First program/TripletCompare.cs
@@ -8,7 +8,9 @@ namespace First_program
 {
     public class TripletCompare
     {
-        //Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+        //Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges,
+        //awarding points on a scale from 1 to 100 for three categories: problem clarity, originality,
+        //and difficulty.
 
         public static List<int> compareTriplets(List<int> a, List<int> b)
         {
